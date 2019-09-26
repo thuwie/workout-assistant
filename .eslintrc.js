02165a9113f300f6cefb8d5e63dd133b98a9e37e
Kirill <8338050@gmail.com>
2019-09-27 00:48:36 +0300

    Edit eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,7 @@ module.exports = {
     "max-len": ["error", 120],
     "no-console": "off",
     "import/prefer-default-export": "off",
+    "no-empty-function": "off",
+    "no-useless-constructor": "off",
   },
 };
