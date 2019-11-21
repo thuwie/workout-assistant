a9baa65854ddad761efe6d203c35cab3c22ee560
Ivan.Poliakov <ipolyiakov@gmail.com>
2019-11-22 00:03:39 +0300

    Add Id to auth responses

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -17,34 +17,35 @@ export class AuthService {
   async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findOne({ username });
     if (!user) { return null; }
-    const { email, password } = user;
+    const { id, email, password } = user;
     const passwordIsOk = await bcrypt
       .compare(pass, password.toString());
     if (passwordIsOk) {
-      return { username, email };
+      return { id, username, email };
     }
     return null;
   }
 
   async login(user: any) {
-    const { username, email } = user;
+    const { id, username, email } = user;
     const jwtPayload = { username, email };
     return {
-      access_token: this.jwtService.sign(jwtPayload),
+      userId: id,
+      accessToken: this.jwtService.sign(jwtPayload),
     };
   }
 
   async signUp(@Body() createUserDto: UserDto) {
     try {
       const { username, email, password } = createUserDto;
-      if (password === null) { return null; }
+      if (password === null || username === null || email === null) { return null; }
       const usr = await this.usersService.findOne({ $or: [{ username }, { email }] });
       if (usr) { return null; }
       const tmpUser = createUserDto;
       tmpUser.password = await bcrypt.hash(password, 10);
-      await this.usersService.create(tmpUser);
+      const newUser = await this.usersService.create(tmpUser);
       const jwtPayload = { username, email };
-      return { access_token: this.jwtService.sign(jwtPayload) };
+      return { userId: newUser.id, accessToken: this.jwtService.sign(jwtPayload) };
     } catch (err) {
       console.log(err);
       return null;
